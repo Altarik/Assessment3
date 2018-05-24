86e498f36aae5e80f1324073eb6b151efa988c61
ALAMI Tarik <alamiouazzani.tarik@gmail.com>
2018-05-24 03:34:20 +0200

    [User story] A user should be able to read Text Files

---
+++ Assessment3/TextReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment3
+{
+    public class TextReader
+    {
+        public string Read(string path)
+        {
+            try
+            {
+                return File.ReadAllText(path);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
