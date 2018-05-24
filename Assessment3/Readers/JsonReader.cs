8cf1b587a8ce5203ea2056eea75512069433e4e3
ALAMI Tarik <alamiouazzani.tarik@gmail.com>
2018-05-24 04:32:31 +0200

    Initialize Json Reader

---
+++ Assessment3/Readers/JsonReader.cs
@@ -0,0 +1,17 @@
+﻿using Assessment3.Encryptions.Interface;
+using Assessment3.Readers.Abstract;
+using Assessment3.SecurityRoles.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment3
+{
+    public class JsonReader
+    {
+       
+    }
+}
