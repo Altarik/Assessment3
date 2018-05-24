edf086bcb6b782bb7dfbb0b5014fac3ddf737937
ALAMI Tarik <alamiouazzani.tarik@gmail.com>
2018-05-24 04:36:02 +0200

    [User story] A user should be able to read JSON Files

--- Assessment3/Readers/JsonReader.cs
+++ Assessment3/Readers/JsonReader.cs
@@ -1,6 +1,8 @@
 ﻿using Assessment3.Encryptions.Interface;
 using Assessment3.Readers.Abstract;
 using Assessment3.SecurityRoles.Interface;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,6 +14,21 @@ namespace Assessment3
 {
     public class JsonReader
     {
-       
+        public string Read(string path)
+        {
+            try
+            {
+                using (StreamReader file = File.OpenText(path))
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    JObject jsonObject = (JObject)JToken.ReadFrom(reader);
+                    return jsonObject.ToString();
+                }
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
     }
 }
