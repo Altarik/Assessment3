7d0f75b4cee3a4676617263b4bbaa03827deb523
ALAMI Tarik <alamiouazzani.tarik@gmail.com>
2018-05-24 04:43:48 +0200

    [User story] A user should be able to read JSON Files on security role based context

--- Assessment3/Readers/JsonReader.cs
+++ Assessment3/Readers/JsonReader.cs
@@ -12,6 +12,34 @@ namespace Assessment3
 {
     public class JsonReader
     {
-       
+        protected ISecurityRole SecurityRole;
+        protected string UserName = string.Empty;
+        protected string RoleName = string.Empty;
+
+        public JsonReader(ISecurityRole securityRole = null, string userName = null, string roleName = null)
+        {
+            SecurityRole = securityRole;
+            UserName = userName;
+            RoleName = roleName;
+        }
+
+        public string Read(string path)
+        {
+            //Set Security Role
+            if (SecurityRole != null)
+            {
+                SecurityRole.SetRights(UserName, RoleName);
+            }
+
+            // Read content
+            string content = RawRead(path);
+            
+            return content;
+        }
+
+        private string RawRead(string path)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
